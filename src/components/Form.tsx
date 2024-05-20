b46bd3cf8f3f1465be792a60917491b4309413eb
Mateus Lopes <mateusbslopes@gmail.com>
2024-05-20 13:46:19 -0300

    feat: add account with only parent and list its real id

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Text, TextInput } from "react-native";
+import { Button, Text, TextInput } from "react-native";
 
 type AccountCode = {
     parent: number;
@@ -7,11 +7,17 @@ type AccountCode = {
     grandchild: number;
 }
 
+type Account = {
+    id: string
+    accounts?: Account[]
+}
+
 const Form = (): JSX.Element => {
 
     const [code, setCode] = useState("")
+    const [suggestedCode, setSuggestedCode] = useState()
 
-    const [accounts, setAccounts] = useState([[[]]])
+    const [accounts, setAccounts] = useState<Account[]>([])
     const [accountCode, setAccountCode] = useState<AccountCode>({
         parent: 0,
         child: 0,
@@ -23,12 +29,11 @@ const Form = (): JSX.Element => {
 
     const inputIsValid = (parent: string, child: string, grandchild: string, rest: string): boolean =>
         Boolean(
-            (parent && !Number.isNaN(Number(parent)) && Number(parent) <= 999) &&
-            (child == undefined || (child != "" && !Number.isNaN(Number(child)) && Number(child) <= 999)) &&
-            (grandchild == undefined || (grandchild != "" && !Number.isNaN(Number(grandchild)) && Number(child) <= 999)) &&
-            (rest == undefined && rest != "") 
+            (parent && !Number.isNaN(Number(parent)) && Number(parent) > 0 && Number(parent) <= 999) &&
+            (child == undefined || (child != "" && !Number.isNaN(Number(child)) && Number(child) > 0 && Number(child) <= 999)) &&
+            (grandchild == undefined || (grandchild != "" && !Number.isNaN(Number(grandchild)) && Number(grandchild) > 0 && Number(child) <= 999)) &&
+            (rest == undefined && rest != "")
         )
-
 
     useEffect(() => {
         const [parent, child, grandchild, rest] = code.split(".")
@@ -45,28 +50,47 @@ const Form = (): JSX.Element => {
             child: Number(child),
             grandchild: Number(grandchild)
         })
-
-
-
     }, [code])
+
+    const addAccount = () => {
+        const realAccountCode = {
+            parent: --accountCode.parent,
+            child: --accountCode.child,
+            grandchild: --accountCode.grandchild
+        }
+
+        accounts[realAccountCode.parent] = {
+            id: `${realAccountCode.parent}`
+        }
+
+        console.log(accounts)
+
+        setAccounts([...accounts])
+    }
+
+    const mapAccount = (acc: Account): any => (
+        acc && [
+            <Text key={acc.id}>{acc.id}</Text>,
+            acc.accounts?.map(mapAccount)
+        ])
+
 
     return (
         <>
             <>
                 <Text>Conta</Text>
-                <TextInput onChangeText={setCode} />
-            </>
-            <>
-                <Text>{code}</Text>
-                <Text>Parent - {accountCode.parent}</Text>
-                <Text>Child - {accountCode.child}</Text>
-                <Text>Grandchild - {accountCode.grandchild}</Text>
+                <TextInput onChangeText={setCode} style={{ borderColor: 'black', borderWidth: 3 }} />
+                <Button title="Add" onPress={addAccount} />
             </>
             <>
                 <Text style={{ color: "red" }}>{error}</Text>
             </>
+            <>{accounts.map(mapAccount)}</>
         </>
     )
+
+
+
 }
 
 export default Form;
