a39c421c087956b4f470eeba73b419f579a3713e
Mateus Lopes <mateusbslopes@gmail.com>
2024-05-21 16:53:01 -0300

    feat: add stack navigation to project

--- App.tsx
+++ App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import Form from './src/components/Form';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 function App(): React.JSX.Element {
 
   return (
-    <Form />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Formulario" component={Form} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
